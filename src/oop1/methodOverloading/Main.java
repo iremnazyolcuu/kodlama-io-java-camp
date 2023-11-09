f2eaa78a71620a3a91b79244c17ea1a539f51c8a
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-11-09 19:46:36 +0300

    oop - method overloading

---
+++ src/oop1/methodOverloading/Main.java
@@ -0,0 +1,13 @@
+package oop1.methodOverloading;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MethodOverloading methodOverloading = new MethodOverloading();
+        System.out.println(methodOverloading.add(3, 7));
+        System.out.println(methodOverloading.add(2, 4, 6));
+
+    }
+
+}
---
+++ src/oop1/methodOverloading/MethodOverloading.java
@@ -0,0 +1,13 @@
+package oop1.methodOverloading;
+
+public class MethodOverloading {
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int add(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+}
