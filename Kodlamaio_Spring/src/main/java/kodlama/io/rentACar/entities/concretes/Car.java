e0257c1edefb3d48c4de43e8b40d792c9def8ebb
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-12-01 16:55:13 +0300

    added entities

---
+++ Kodlamaio_Spring/src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
@@ -0,0 +1,37 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cars")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "plate", unique = true)
+    private String plate;
+
+    @Column(name = "dailyPrice")
+    private double dailyPrice;
+
+    @Column(name = "modelYear")
+    private int modelYear;
+
+    @Column(name = "state")
+    private int state; // 1-Available, 2-Rented, 3-Maintenance
+
+    @ManyToOne
+    // iki tablo birbiriyle nasıl join oluyor: modeld_id ile
+    @JoinColumn(name = "model_id")
+    private Model model;
+
+}
